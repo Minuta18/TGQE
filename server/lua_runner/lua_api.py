25cfd79c10b2f34fa3be8845e9f030ccd8a105f2
Minuta18 <ig.samsonov10@yandex.ru>
2024-10-23 10:23:11 +0300

    Lots of new functional in LuaApi class

--- server/lua_runner/lua_api.py
+++ server/lua_runner/lua_api.py
@@ -7,13 +7,19 @@ class LuaApi:
     Class for collecting information about API methods
     '''
     
-    def __init__(self):
+    def __init__(self, table: str = None):
         '''
         Lua API class
     
         Class for collecting information about API methods
+        
+        Args:
+            table: str - table, where methods are stored
         '''
+        
+        self._table = table
         self._methods = dict()
+        self._included_apis = list()
         
     def register_api_method(
         self,
@@ -37,6 +43,8 @@ class LuaApi:
         Returns:
             None
         '''
+        if self._table is not None:
+            lua_name = f'{self._table}.{lua_name}'
         if self._methods.get(lua_name) is not None:
             raise IndexError(f'lua_name={lua_name} is already used')
         self._methods[lua_name] = py_method
@@ -60,6 +68,8 @@ class LuaApi:
             None
         '''
 
+        if self._table is not None:
+            lua_name = f'{self._table}.{lua_name}'
         self._methods[lua_name] = py_method
         
     def get_all_methods(self) -> dict[
@@ -68,8 +78,113 @@ class LuaApi:
         '''
         Returns all methods
         
-        Returns all methods and represent as dict
+        Returns all methods and represents it as dict
         '''
         
-        return self._methods
+        methods_to_return = self._methods
+        for api in self._included_apis:
+            apis_methods = api.get_all_methods()
+            apis_methods_reformated = dict()
+            for method in apis_methods.keys():
+                apis_methods_reformated[f'{self._table}.{method}'] = \
+                    apis_methods[method]
+            methods_to_return.update(apis_methods_reformated)
+            
+        return methods_to_return
         
+    def get_table_name(self) -> str:
+        '''
+        Returns name of the table, where methods will be created
+        
+        Returns:
+            Name of the table
+        '''
+        
+        return self._table
+    
+    def set_table_name(self, new_table_name: str) -> None:
+        '''
+        Changes the name of the table, where methods will be created
+        
+        Args:
+            new_table_name: str - new name of the table
+        '''
+                
+        for method in self._methods.keys():
+            new_name = method.removesuffix(self._table + '.')
+            if new_table_name is not None:
+                new_name = f'{new_table_name}.{new_name}'
+            self._methods[new_name] = self._methods[method]
+            self._methods.pop(method, None)
+    
+        self._table = new_table_name
+    
+    def include_api(self, api: typing.Self) -> None:
+        '''
+        Includes all methods from the given api to this.
+        
+        If current table is specified, then all methods from the `api` will be 
+        included in this table. E. g.
+        
+        ```python
+        self._table = 'nice_table'
+        self._methods = {
+            'nice_table.nice_method': some_method,
+        }
+        
+        some_api._table = 'cool_table'
+        some_api._methods = {
+            'cool_table.cool_method': some_method2,
+        }
+        
+        self.include_api(some_api)
+        
+        # Will become to this:
+        
+        self._table = 'nice_table'
+        self._methods = {
+            'nice_table.nice_method': some_method,
+            'nice_table.cool_table.cool_method': some_method2,
+        }
+        ```
+    
+        Also it's important to note that methods will be included only in 
+        `get_all_methods()` method. So it stores just a *pointer* to a api, not
+        api itself.
+        
+        Args:
+            api: LuaApi - api to include
+            
+        Returns:
+            None
+        '''
+    
+        self._included_apis.append(api)
+        
+    def api_method(
+        self, 
+        func: typing.Callable[..., typing.Any], 
+        lua_name: str
+    ) -> typing.Callable[..., typing.Any]:
+        '''
+        Api method decorator
+        
+        Decorator which will add function as lua method automatically. Can be 
+        used as following:
+        
+        ```python
+        @your_api.api_method('example_method')
+        def example_method(a: int, b: int) -> int:
+            return a + b
+        ```
+        
+        Args:
+            lua_name: str - name in lua (can be different)
+        '''
+        
+        def wrapper(*args, **kwargs):
+            self.register_api_method(lua_name, func)
+            return func(*args, **kwargs)
+        return wrapper
+
+    
