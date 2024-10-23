3175d7e2db5cebd14e7d5a71202c395cf69c2e5d
M-Stepan-373 <step3212m@gmail.com>
2024-10-23 12:19:14 +0300

    added tg bot code (doesnt tested)

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from bot_main import TG_bot
 
 logger = logging.getLogger('main')
 
@@ -30,3 +32,9 @@ function cool_sum(a, b)
 end
 ''')
     print(cont.call_func('cool_sum', args=(5, 3))) # Calls function from lua
+
+    def request_to_server(message):
+        return [['aboba', None]], [[None]]
+
+    bot = TG_bot(token=os.environ.get('TOKEN'), request_to_server=request_to_server)
+    bot.start()
